d78cc45f911851729db97711af444ec5bebb7314
hallowf <alexandre.cortegaca@gmail.com>
2019-10-08 15:15:02 +0100

    Updated tests

--- tests/test_verifiers.py
+++ tests/test_verifiers.py
@@ -1,31 +1,52 @@
+import os
 import unittest
 
 from argparse import Namespace
 
-from src.cmd_main import verify_required
+from src.core.sorter import FileSorter
+from src.core.utils import verify_required
 from src.core.custom_exceptions import WhyWouldYou, DirMissing, OutDirNotEmpty
 
 
-class TestCLIVerifier(unittest.TestCase):
+class TestFileSorterVerifier(unittest.TestCase):
 
+    # This only runs once before all tests
+    # unlike setUp that runs before every test
     @classmethod
     def setUpClass(cls):
-      if not os.path.isdir("TEST_IMAGES"):
-        os.mkdir("TEST_IMAGES")
-      if not os.path.isdir("TEST_OUTPUT"):
-        os.mkdir("TEST_OUTPUT")
-
+        if not os.path.isdir("TEST_IMAGES"):
+            os.mkdir("TEST_IMAGES")
+        if not os.path.isdir("TEST_OUTPUT"):
+            os.mkdir("TEST_OUTPUT")
 
     def test_raises_on_missing_source(self):
         """
-        Test that check ifs an exception is raised
+        Test that checks if an exception is raised
+        when the source directory does not exist
+        """
+        with self.assertRaises(DirMissing):
+            FileSorter("/a_path_that_does_not_exist", "./OUTPUT_IMAGES")
+
+    def test_raises_on_insufficient_files(self):
+        """
+        Checks that there are enough files
+        in the source directory to process
+        """
+        with self.assertRaises(WhyWouldYou):
+            FileSorter("./TEST_IMAGES", "./TEST_OUTPUT")
+
+
+class TestUtilsVerifier(unittest.TestCase):
+    def test_raises_on_missing_source(self):
+        """
+        Test that checks if an exception is raised
         when the source directory does not exist
         """
         args = Namespace(from_dir="/a_path_that_does_not_exist",
                          skip_copy=False,
                          sort_remaining=False,
                          split_by=None,
-                         to_dir="")
+                         to_dir="./TEST_OUTPUT")
         with self.assertRaises(DirMissing):
             verify_required(args)
 
@@ -38,10 +59,9 @@ class TestCLIVerifier(unittest.TestCase):
                          skip_copy=False,
                          sort_remaining=False,
                          split_by=None,
-                         to_dir="")
+                         to_dir="./TEST_OUTPUT")
         with self.assertRaises(WhyWouldYou):
             verify_required(args)
-
 
 
 if __name__ == "__main__":
