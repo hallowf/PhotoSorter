48f3390c1d3375b28c15bf29eb0cb8f428ad2f12
hallowf <alexandre.cortegaca@gmail.com>
2020-04-14 19:06:46 +0100

    re-added function to write images, create unknown date folder and create new folder

--- src/core/sorter.py
+++ src/core/sorter.py
@@ -99,23 +99,26 @@ class FileSorter(object):
                 # check and yield progress
                 self.file_counter += 1
                 if(round(self.file_counter % self.one_percent_files, 1) == 0.1):
-                    self.logger.info(str(self.file_counter) + " / " + self.totalAmountToCopy + " processed.")
-                    yield(str(self.file_counter) + " / " + self.totalAmountToCopy + " processed.\n")
+                    msg = "%s / %s processed." % (self.file_counter, self.file_number)
+                    self.logger.info(msg)
+                    yield(msg + "\n")
 
         # this needs to be moved onto another function to avoid a high cyclomatic complexity
         final_dest = os.path.join(self.dest, "PROCESSED")
         self.logger.info("Sorting images in %s by date" % (final_dest))
-        for img in self.postprocess_images(final_dest, self.min_evt_delta_days, self.split_months):
+        for img in self.postprocess_images(final_dest):
             print(img)
 
     # Iterates trough the processed image dir, processes each individual file
-    # and yield the processed image
-    def postprocess_images(self, image_dir, min_evt_delta_days, split_by_month):
+    # and yields the processed image
+    def postprocess_images(self, image_dir):
         for root, dirs, files in os.walk(image_dir):
             for file in files:
                 extension = os.path.splitext(file)[1][1:].lower()
                 if extension  in self.known_image_extensions:
-                    yield self.postprocess_image(image_dir, file)
+                    processed_image = self.postprocess_image(image_dir, file)
+                    if processed_image != None:
+                        yield processed_image
                 else:
                     print("Skipping file %s, not an image" % file)
 
@@ -129,7 +132,8 @@ class FileSorter(object):
         except Exception as e:
             # self.logger.warning("invalid exif tags for %s" % (file_name))
             # self.logger.debug("Exception: %s" % (str(e)))
-            yield "invalid exif tags for %s\n" % (file_name)
+            image.close()
+            return None
 
         # distinct different time types
         if creation_time is None:
@@ -142,3 +146,70 @@ class FileSorter(object):
                 creation_time = localtime(os.path.getctime(image_path))
         return (mktime(creation_time), image_path)
         image.close()
+
+    def write_image(self, img_tuple, dest_root, min_evt_delta_days, split_by_month=False):
+        min_evt_delta = min_evt_delta_days * 60 * 60 * 24  # convert in seconds
+        previous_time = None
+        evt_number = 0
+        previous_dest = None
+        c_time = datetime.now()
+        today = c_time.strftime("%d/%m/%Y")
+
+        dest = ""
+        dest_file_path = ""
+        t = localtime(img_tuple[0])
+        year = strftime("%Y", t)
+        month = split_by_month and strftime("%m", t) or None
+        creation_date = strftime("%d/%m/%Y", t)
+        file_name = ntpath.basename(img_tuple[1])
+
+        if(creation_date == today):
+            if self.sort_remaining:
+                dest = os.path.join(dest_root, "unknown-to-sort")
+                if not os.path.isdir(dest):
+                    os.mkdir(dest)
+                dest_file_path = os.path.join(dest, file_name)
+            else:
+                self.create_unknown_date_folder(dest_root)
+                dest = os.path.join(dest_root, "unknown")
+                dest_file_path = os.path.join(dest, file_name)
+        else:
+            self.logger.debug("%s was created in %s" % (file_name, creation_date))
+            if (previous_time is None) or ((previous_time + min_evt_delta) < img_tuple[0]):
+                evt_number = evt_number + 1
+                self.create_new_folder(dest_root, year, month, evt_number)
+
+            previous_time = img_tuple[0]
+
+            dest_components = [dest_root, year, month, str(evt_number)]
+            dest_components = [v for v in dest_components if v is not None]
+            dest = os.path.join(*dest_components)
+
+            # it may be possible that an event covers 2 years.
+            # in such a case put all the images to the event in the old year
+            if not (os.path.exists(dest)):
+                dest = previous_dest
+                # dest = os.path.join(dest_root, str(int(year) - 1), str(evt_number))
+
+            previous_dest = dest
+            dest_file_path = os.path.join(dest, file_name)
+
+        if not (os.path.exists(dest_file_path)):
+            shutil.move(img_tuple[1], dest)
+        else:
+            if (os.path.exists(img_tuple[1])):
+                os.remove(img_tuple[1])
+
+
+    def create_unknown_date_folder(self, dest_root):
+        path = os.path.join(dest_root, "unknown")
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def create_new_folder(self, dest_root, year, month, evt_number):
+        if month is not None:
+            new_path = os.path.join(dest_root, year, month, str(evt_number))
+        else:
+            new_path = os.path.join(dest_root, year, str(evt_number))
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
